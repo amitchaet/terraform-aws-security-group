8ca8ee68b1adf8224143b8bde40836769a3f51a7
amit.chait <amchait11@gmail.com>
2025-05-19 08:59:57 +0300

    sg module

---
+++ variables.tf
@@ -0,0 +1,30 @@
+variable "project_name" {
+  description = "Name of the project"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "VPC ID to create the security group in"
+  type        = string
+}
+
+variable "ingress_rules" {
+  description = "List of ingress rules"
+  type = list(object({
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+  }))
+}
+
+variable "egress_rules" {
+  description = "List of egress rules"
+  type = list(object({
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+  }))
+}
+
